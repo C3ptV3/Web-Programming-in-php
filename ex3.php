448415f81cf45793c20d850d4285024f39365c9f
C3ptV3 <24461272+C3ptV3@users.noreply.github.com>
2019-03-13 11:01:56 +0100

    Create ex3.php
    Mix of php Html

---
+++ ex3.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="Content-Type" Content="text/html,charset=iso-8859-2">
+<TITLE>Moje imie i nazwisko</TITLE>
+</HEAD>
+<BODY>
+<?php
+$imie="Jan";
+$nazwisko="Kowalski";
+print("Nazywam sie"." ".$imie." ".$nazwisko);
+?>
+</BODY>
+<HTML>
