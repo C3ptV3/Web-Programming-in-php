8b2ddf1af9d461efbf6ca6ab8cb8e6fff7cd447f
C3ptV3 <24461272+C3ptV3@users.noreply.github.com>
2019-06-13 19:35:29 +0200

    Update ex11.php

--- ex11.php
+++ ex11.php
@@ -7,47 +7,9 @@
 <TABLE border="1">
 <TR><TH> a </TH><TH>a<sup>2</sup></TH><TH>a<sup>3</sup></TH><TH>a<sup>4</sup></TH><TH>a<sup>5</sup></TH><TH>a<sup>6</sup></TH><TH>a<sup>7</sup></TH></TR>
 <?php
-for($i=1;$i<=30;$i++)
-{
-    $cub2=$i*$i;
-    $cub3=$i*$i*$i;
-    $cub4=$i*$i*$i*$i;
-    $cub5=$i*$i*$i*$i*$i;
-    $cub6=$i*$i*$i*$i*$i*$i;
-    $cub7=$i*$i*$i*$i*$i*$i*$i;
-//print"<TR><TD>$i</TD><TD>$sqrt</TD><TD>$sqr</TD><TD>$cub</TD></TR><br>";
-print"<TR><TD>$i</TD><TD>$cub2</TD><TD>$cub3</TD><TD>$cub4</TD><TD>$cub5</TD><TD>$cub6</TD><TD>$cub7</TD></TR>\n";
-}
 
-
-
-echo"<br>";
-$x=50;
-$y=1;
-echo "<pre>";
-for($i=1; $i<=$x; $i++)
-            {
-
-                for($bosluq=0;$bosluq<=$x-$i;$bosluq++){
-                    echo" ";
-                }
-
-                
-
-                for($ulduz=1;$ulduz<=$i;$ulduz++){
-                    echo "*";
-                    echo" ";
-                }
-
-
-                for($bosluq=0;$bosluq<=$x-$i;$bosluq++){
-                    echo" ";
-                }
-
-                echo"<br>";
-
-            }
-echo"</pre>";                          
+$a1=10,$a2=5;
+echo"Elchin Habibov"
 
 
 
